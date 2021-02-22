9183e2ac73c80e120f3fc573daaacb445e59bc7b
Jean-Marin Chaintron <46138593+jamesdevgithub@users.noreply.github.com>
2021-02-22 21:30:40 +0100

    brand new not that buggy source

--- main.cpp
+++ main.cpp
@@ -1,225 +1,229 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cassert>
+#include <vector>
+#include <ctime>
 
-using namespace std;
-using namespace sf;
+const int TILE_SIZE = 100;
+const int GRID_SIZE = 8;
+std::vector<std::vector<int>> grid;	// Stores the power of 2
+sf::Sprite cell;
+sf::Texture texture;
+bool didDown = false;	// for automated play
 
-int grid[8][8];
-bool playing = false;
-RenderWindow window(VideoMode(800, 800), "2048");
-bool gridModified = true;
-const float tileSize = 100.0f;
-Texture texture;
-map<int, Sprite> tiles;
-bool gameover = false;
-
-void printGrid()
+bool putRandom(const int nbBlocks, const std::vector<int>& powers)
 {
-	for (int y = 0; y < 8; ++y)
-    {
-        for (int x = 0; x < 8; ++x)
-            cout << grid[x][y] << " ";
-        cout << "\n";
-    }
-    cout << "---------------------------------" << endl;
-}
+	assert(nbBlocks > 0);
+	assert(nbBlocks == powers.size());
 
-void swipeUp()
-{
+	int freeLocations = 0;
 	for (auto& col : grid)
-        for (int i = 1; i < 8; ++i)
-        {
-        	int j = i;
-        	while (j - 1 >= 0 && (col[j - 1] == 0 || col[j - 1] == col[i])) j--;
-
-        	if (j != i)
-            {
-                col[j] += col[i];
-                col[i] = 0;
-            }
-        }
-}
-
-void swipeDown()
-{
-	for (auto& col : grid)
-        for (int i = 6; i >= 0; --i)
-        {
-        	int j = i;
-        	while (j + 1 < 8 && (col[j + 1] == 0 || col[j + 1] == col[i])) j++;
-
-        	if (j != i)
-            {
-                col[j] += col[i];
-                col[i] = 0;
-            }
-        }
-}
-
-void swipeLeft()
-{
-	for (int rowNumber = 0; rowNumber < 8; ++rowNumber)
-    {
-        int* row[8];
-        for (int k = 0; k < 8; ++k)
-            row[k] = &grid[k][rowNumber];
-
-        for (int i = 1; i < 8; ++i)
-        {
-            int j = i;
-            while (j - 1 >= 0 && (*row[j - 1] == 0 || *row[j - 1] == *row[i])) j--;
-
-            if (j != i)
-            {
-                *row[j] += *row[i];
-                *row[i] = 0;
-            }
-        }
-    }
-}
-
-void swipeRight()
-{
-	for (int rowNumber = 0; rowNumber < 8; ++rowNumber)
-    {
-        int* row[8];
-        for (int k = 0; k < 8; ++k)
-            row[k] = &grid[k][rowNumber];
-
-        for (int i = 6; i >= 0; --i)
-        {
-            int j = i;
-            while (j + 1 < 8 && (*row[j + 1] == 0 || *row[j + 1] == *row[i])) j++;
-
-            if (j != i)
-            {
-                *row[j] += *row[i];
-                *row[i] = 0;
-            }
-        }
-    }
-}
-
-void addRandom()
-{
-	int nbFreeCells = 0;
-	for (int i = 0; i < 8; ++i)
-        nbFreeCells += count(&grid[i][0], &grid[i][0] + 8, 0);
-
-    if (nbFreeCells == 0)
-    {
-    	gameover = true;
-    	cout << "game over" << endl;
-    	return;
-    }
-
-	int randomIndex = rand() % nbFreeCells;
-    int counter = 0;
-    for (int i = 0; i < 8; ++i)
-    {
-    	for (int j = 0; j < 8; ++j)
-    	{
-    		if (grid[i][j] == 0 && counter++ == randomIndex)
-    			grid[i][j] = (rand() % 2 == 0) ? 2 : 4;
-    	}
-    }
-}
-
-void init()
-{
-	srand(time(0));
-	texture.loadFromFile("tiles.png");
-	tiles[0] = Sprite(texture, IntRect(0, 0, tileSize, tileSize));
-	for (int i = 1; i <= 11; ++i)
-		tiles[pow(2, i)] = Sprite(texture, IntRect(i * tileSize, 0, tileSize, tileSize));
-    window.setFramerateLimit(30);
-}
-
-void handleEvents()
-{
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-        if (event.type == Event::Closed)
-            window.close();
-
-        else if (event.type == Event::KeyPressed)
-        {
-        	if (!gameover && playing)
-        	{
-        		switch (event.key.code)
-        		{
-        			case Keyboard::Up:
-		                swipeUp();
-		                break;
-
-		            case Keyboard::Down:
-		                swipeDown();
-		                break;
-
-		            case Keyboard::Left:
-		                swipeLeft();
-		                break;
-
-		            case Keyboard::Right:
-		                swipeRight();
-		                break;
-
-                    default:
-                        break;
-        		}
-
-        		gridModified = true;
-        		playing = false;
-        	}
-
-            if (event.key.code == Keyboard::D)
-            	printGrid();
-        }
-    }
-}
-
-void update()
-{
-	if (!playing)
+		freeLocations += std::count(col.begin(), col.end(), 0);
+	
+	for (int k = 0; k < nbBlocks; ++k)
 	{
-		addRandom();
-		playing = true;
-		gridModified = true;
+		if (freeLocations == 0)
+			return false;
+
+		int randomCell = rand() % freeLocations;
+		int counter = 0;
+
+		for (int i = 0; i < GRID_SIZE; ++i)
+		{
+			for (int j = 0; j < GRID_SIZE; ++j)
+			{
+				if (grid[i][j] != 0)
+					continue;
+				else if (counter++ == randomCell)
+					grid[i][j] = powers[k];
+			}
+		}
+
+		--freeLocations;
+	}
+
+	return true;
+}
+
+void moveDown()
+{
+	for (auto& col : grid)
+	{
+		for (int y = GRID_SIZE - 2; y >= 0; --y)
+		{
+			for (int z = y; z < GRID_SIZE - 1; ++z)
+			{
+				int& current = col[z];
+				int& under = col[z + 1];
+
+				if (under == 0)
+					std::swap(current, under);
+				else if (under == current)
+				{
+					++under;
+					current = 0;
+				}
+			}
+		}
 	}
 }
 
-void render()
+void moveUp()
 {
-    if (gridModified)
-    {
-        printGrid();
-        gridModified = false;
-    }
+	for (auto& col : grid)
+	{
+		for (int y = 1; y < GRID_SIZE; ++y)
+		{
+			for (int z = y; z > 0; --z)
+			{
+				int& current = col[z];
+				int& above = col[z - 1];
 
-    window.clear();
+				if (above == 0)
+					std::swap(current, above);
+				else if (above == current)
+				{
+					++above;
+					current = 0;
+				}
+			}
+		}
+	}
+}
 
-    for (int i = 0; i < 8; ++i)
-    {
-    	for (int j = 0; j < 8; ++j)
-    	{
-    		tiles[ grid[i][j] ].setPosition(i * tileSize, j * tileSize);
-    		window.draw(tiles[ grid[i][j] ]);
-    	}
-    }
+void moveLeft()
+{
+	for (int row = 0; row < GRID_SIZE; ++row)
+	{
+		for (int x = 1; x < GRID_SIZE; ++x)
+		{
+			for (int z = x; z > 0; --z)
+			{
+				int& current = grid[z][row];
+				int& left = grid[z - 1][row];
 
-    window.display();
+				if (left == 0)
+					std::swap(current, left);
+				else if (left == current)
+				{
+					++left;
+					current = 0;
+				}
+			}
+		}
+	}
+}
+
+void moveRight()
+{
+	for (int row = GRID_SIZE - 1; row >= 0; --row)
+	{
+		for (int x = GRID_SIZE - 2; x >= 0; --x)
+		{
+			for (int z = x; z < GRID_SIZE - 1; ++z)
+			{
+				int& current = grid[z][row];
+				int& right = grid[z + 1][row];
+
+				if (right == 0)
+					std::swap(current, right);
+				else if (right == current)
+				{
+					++right;
+					current = 0;
+				}
+			}
+		}
+	}
+}
+
+void printConsole()
+{
+	for (int j = 0; j < GRID_SIZE; ++j)
+	{
+		for (int i = 0; i < GRID_SIZE; ++i)
+			std::cout << grid[i][j];
+		std::cout << '\n';
+	}
+	std::cout << "----------------------------------------\n";
 }
 
 int main()
 {
-	init();
+	using namespace std;
+	srand(static_cast<unsigned int>(time(0)));
+
+	// Initialization
+	sf::RenderWindow window(sf::VideoMode(TILE_SIZE * GRID_SIZE, TILE_SIZE * GRID_SIZE), "two thousands fourty eight");
+	//window.setVerticalSyncEnabled(true);
+	texture.loadFromFile("tiles.png");
+	cell.setTexture(texture);
+
+	grid.resize(GRID_SIZE);
+	for (auto& col : grid)
+		col.resize(GRID_SIZE);
+
+	// Fills the grid
+	putRandom(2, std::vector<int>(2, 1));
 
 	while (window.isOpen())
 	{
-		handleEvents();
-		update();
-		render();
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+			else if (event.type == sf::Event::KeyPressed)
+			{
+				bool played = true;
+
+				if		(event.key.code == sf::Keyboard::Down)	moveDown();
+				else if (event.key.code == sf::Keyboard::Up)	moveUp();
+				else if (event.key.code == sf::Keyboard::Left)	moveLeft();
+				else if (event.key.code == sf::Keyboard::Right)	moveRight();
+				else											played = false;
+
+				if (played)
+				{
+					if (!putRandom(1, { rand() % 2 }))
+						std::cout << "The Game is Up!" << std::endl;
+				}
+			}
+				
+		}
+
+		if (didDown)
+		{
+			moveRight();
+			putRandom(1, { rand() % 2 });
+			didDown = false;
+		}
+		else
+		{
+			moveDown();
+			putRandom(1, { rand() % 2 });
+			didDown = true;
+		}
+
+		window.clear();
+
+		// Draw
+		for (int i = 0; i < GRID_SIZE; ++i)
+		{
+			for (int j = 0; j < GRID_SIZE; ++j)
+			{
+				if (int power = grid[i][j]; power != 0)
+				{
+					cell.setPosition((float)i * TILE_SIZE, (float)j * TILE_SIZE);
+					int xTextureCoords = power * TILE_SIZE;
+					cell.setTextureRect(sf::IntRect(xTextureCoords, 0, TILE_SIZE, TILE_SIZE));
+					window.draw(cell);
+				}
+			}
+		}
+
+		window.display();
 	}
 
 	return 0;
